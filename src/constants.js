8abb47694410fef20017969fb321cf89a60fe79f
unknown <bhumika.bsharma03@gmail.com>
2024-10-14 22:48:33 +0530

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,34 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in Python and C++ these days. I've worked on projects related to web development using Django and data analysis with Python. 
+  I’m also diving into NLP and AI. Regardless of the language, I just enjoy solving problems through programming.`,
+  
+  "cs-degree": `This is my B.Sc. (Hons.) Electronics degree. 
+  I graduated from Shaheed Rajguru College of Applied Sciences for Women, University of Delhi. While it wasn't solely focused on computer science, it gave me a strong foundation in electronics, problem-solving, and programming concepts. This inspired me to pursue a Master's in Informatics to deepen my knowledge in areas like AI, and web development.`,
+
+  "sofa-table": `That's my sofa. I like to relax here. 
+  I often spend time learning new technologies through YouTube, whether it's about programming or working on improving my problem-solving skills. Sometimes, 
+  I also enjoy watching tech content creators for inspiration and insights.`,
+  
+  tv: `That is my TV. I often use it to take online courses in MySQL, data structures and algorithms (DSA), and Django. 
+  These courses help me enhance my skills while I also enjoy watching movies and series to relax and unwind from my studies and projects!`,
+  
+  bed: `This is where I sleep. I often get great ideas lying here, and when inspiration strikes, I write them down to clear my mind and get some rest.`,
+  
+  resume: `This is my desk, and on it is my resume. <a href="https://drive.google.com/file/d/1MG96QDx3ABvkie4M_Ou5XmIOgopy1Ym8/view?usp=sharing" target="_blank">Check it out?</a> 
+  Contact me at bhumika.bsharma03@gmail.com if you have any interesting job opportunities!`,
+
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+  
+  library: `I have a lot of programming books and some novels on my shelves. I often buy technical books with the best intentions but rarely finish them. Does anyone else have a collection like that?`,
+  
+  exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
+
+};
+
+
+
+
+
